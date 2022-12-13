4f8939d3c163497dfc7e00ca129f04c900cd8cf8
SosnovskayaUlyana <sosnov.skaya@outlook.com>
2022-12-13 15:14:26 +0300

    creating a program with sorting

---
+++ Сортировки/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,162 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include "time.h"
+#include <locale.h>
+
+
+void* malloc(size_t size);
+
+float* creature(int size)
+{
+    float* mas;
+    mas = (float*)malloc(sizeof(float*) * size);
+    for (int i = 0; i < size; i++)
+       mas[i] = rand() + ((float)rand() / ((float)RAND_MAX));
+    return mas;
+}
+
+void swap(float* a, float* b)
+{
+    float tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubble(float* array, int size)
+{
+    int flag = 0;
+    for (int i = 0; i < size; i++)
+    {
+        flag = 0;
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                swap(&array[j], &array[j + 1]);
+                flag = 1;
+            }
+
+        }
+        if (flag == 0) return;
+    }
+
+}
+
+void Insert(float* array, int size)
+{
+    for (int i = 0; i < size; i++)
+        for (int j = i; j > 0; j--)
+            if (array[j] < array[j - 1])
+                swap(&array[j], &array[j - 1]);
+            else break;
+    for (int i = 0; i < size; i++)
+        printf("%f ", array[i]);
+}
+
+int partition(float* mas, int l, int r)
+{
+    int tmp = l + (r - l) / 2;
+    float  mid = mas[tmp];
+    while (l < r)
+    {
+
+        while ((mas[l] < mid) && (l != r))
+            l++;
+        while ((mas[r] > mid) && (r != l))
+            r--;
+        if (mas[r] == mid && r != tmp && r != l)
+        {
+            swap(&mas[r], &mas[l]);
+            r--;
+        }
+        else if (mas[l] == mid && l != tmp && r != l)
+        {
+            swap(&mas[r], &mas[l]);
+            l++;
+        }
+        else swap(&mas[r], &mas[l]);
+        
+    }
+    return r;
+}
+void comb(float* arr, int size)
+{
+    float factor = 1.246;
+    int step = size - 1;
+    while (step != 0)
+    {
+        for (int i = 0; i + step < size; i++)
+            if (arr[i] > arr[i + step])
+                swap(&arr[i], &arr[i + step]);
+        step /= factor;
+    }
+
+}
+
+void  Quicksort(float* array, int left, int right)
+{
+    if (left <= right)
+    {
+        int p = partition(array, left, right);
+        Quicksort(array, left, p-1);
+        Quicksort(array, p+1, right);
+    }
+
+}
+
+int main()
+{
+    clock_t start, finish;
+    int n, vibor, itog, nachalo;
+    float* arr;
+    float* arr1;
+    setlocale(LC_ALL, "Rus");
+    printf("Выберите сортировку\n");
+    printf("1 - сортировка пузырьком\n");
+    printf("2 - сортировка вставками\n");
+    printf("3 - сортировка Хоара\n");
+    printf("4 - сортировка расческой\n");
+    scanf_s("%i", &vibor);
+    printf("Введите количество элементов\n");
+    scanf_s("%i", &n);
+    arr = creature(n);
+    printf("5 - Вывод массива \n");
+    scanf_s("%i", &nachalo);
+    if (nachalo == 5)
+        for (int i = 0; i < n; i++)
+            printf("%f ", arr[i]);
+    printf("\n");
+    if (vibor == 1)
+    {
+        start = clock();
+        bubble(arr, n);
+        finish = clock();
+    }
+    else if (vibor == 2)
+    {
+        start = clock();
+        Insert(arr, n);
+        finish = clock();
+    }
+    else if (vibor == 3)
+    {
+        start = clock();
+        Quicksort(arr, 0, n-1);
+        finish = clock(); 
+    }
+    else if (vibor == 4)
+    {
+        start = clock();
+        comb(arr, n);
+        finish = clock();
+    }
+    printf("6 - Вывод отсортированного массива \n");
+    scanf_s("%i", &itog);
+    if (itog == 6)
+        for (int i = 0; i < n; i++)
+            printf("%f ", arr[i]);
+    printf("\n");
+    printf("Время, коорое потребовалось : ");
+    printf("%f\n", (finish - start) / (double)(CLOCKS_PER_SEC));
+    return 0;
+}
