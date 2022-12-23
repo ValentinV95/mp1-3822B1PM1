63f78b9a752cfedb75d32a78f152dc92d8313e95
SmartyGeeky <69428320+SmartyGeeky@users.noreply.github.com>
2022-12-23 11:00:03 +0300

    Update SortsUnn.cpp

--- SortsUnn.cpp
+++ SortsUnn.cpp
@@ -22,13 +22,15 @@ void bubble_sort(float* arr, int n) {
     int flag = 0;
     for (i = 0; i < n; i++) {
         flag = 0;
-        for (j = 0; j < n - 1-i; j++) {
+        for (j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(&arr[j], &arr[j + 1]);
+                flag = 1;
             }
         }
     }
-
+    if (flag == 0)
+        return;
 }
 
 
