fe993e31285fa4c1954d953860dacf827de45d56
KorotinEgor <251251251azz@gmail.com>
2023-04-06 22:55:14 +0300

    Lab2

---
+++ Korotin/Lab2/func.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+const float M_E = 2.718281828459045;
+
+struct res {
+    float str;
+    float rev;
+};
+
+float mysin(float inp, float x, int i, int mod);
+float mycos(float inp, float x, int i, int mod);
+float myexp(float inp, float x, int i, int mod);
+float ln(float inp, float x, int i, int mod);
+struct res sum(float first, float x, int numb, float (*fun)(float, float, int, int));
