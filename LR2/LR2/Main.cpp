85c48289d9e9182bd5b5e2335f1725d8e9fd749a
ARBUZNIJ <120045631+ARBUZNIJ@users.noreply.github.com>
2023-04-10 23:19:05 +0300

    Update Main.cpp
    + cleaned prog

--- LR2/LR2/Main.cpp
+++ LR2/LR2/Main.cpp
@@ -9,36 +9,36 @@ int main()
 	setlocale(LC_ALL, "Rus");
 	int action, quan;
 	float x;
-	printf("Введите аргумент функции: \n");
-	scanf_s("%lf", &x);
-	printf("Введите кол-во слагаемых для посчёта: \n");
+	printf("Г‚ГўГҐГ¤ГЁГІГҐ Г Г°ГЈГіГ¬ГҐГ­ГІ ГґГіГ­ГЄГ¶ГЁГЁ: \n");
+	scanf_s("%f", &x);
+	printf("Г‚ГўГҐГ¤ГЁГІГҐ ГЄГ®Г«-ГўГ® Г±Г«Г ГЈГ ГҐГ¬Г»Гµ Г¤Г«Гї ГЇГ®Г±Г·ВёГІГ : \n");
 	scanf_s("%d", &quan);
-	printf("Выберите функцию: \n1) sin(x)\n2) cos(x)\n3) ln(x+1)\n4) e^x\n");
+	printf("Г‚Г»ГЎГҐГ°ГЁГІГҐ ГґГіГ­ГЄГ¶ГЁГѕ: \n1) sin(x)\n2) cos(x)\n3) ln(x+1)\n4) e^x\n");
 	scanf_s("%d", &action);
 	switch (action)
 	{
 	case 1:
 	{
-		printf("%.10f %.10f \n", sum(slag_sin, x, quan), sin(float(x)));
-		printf("%.10f %.10f \n", r_sum(r_slag_sin, x, the_last_of_us(slag_sin, x, &quan), quan), sin(float(x)));
+		printf("%.10f %.10f \n", sum(slag_sin, x, quan), sin(x));
+		printf("%.10f %.10f \n", r_sum(r_slag_sin, x, the_last_of_us(slag_sin, x, &quan), quan), sin(x));
 		break;
 	}
 	case 2:
 	{
-		printf("%.10f %.10f \n", sum(slag_cos, x, quan), cos(float(x)));
-		printf("%.10f %.10f \n", r_sum(r_slag_cos, x, the_last_of_us(slag_cos, x, &quan), quan), cos(float(x)));
+		printf("%.10f %.10f \n", sum(slag_cos, x, quan), cos(x));
+		printf("%.10f %.10f \n", r_sum(r_slag_cos, x, the_last_of_us(slag_cos, x, &quan), quan), cos(x));
 		break;
 	}
 	case 3:
 	{
-		printf("%.10f %.10f \n", sum(slag_ln, x, quan), log(float(x)+1));
-		printf("%.10f %.10f \n", r_sum(r_slag_ln, x, the_last_of_us(slag_ln, x, &quan), quan), log(float(x) + 1.0f));
+		printf("%.10f %.10f \n", sum(slag_ln, x, quan), log(x + 1.0f));
+		printf("%.10f %.10f \n", r_sum(r_slag_ln, x, the_last_of_us(slag_ln, x, &quan), quan), log(x + 1.0f));
 		break;
 	}
 	case 4:
 	{
-		printf("%.10f %.10f \n", sum(slag_exp, x, quan), pow(M_E,float(x)));
-		printf("%.10f %.10f \n", r_sum(r_slag_exp, x, the_last_of_us(slag_exp, x, &quan), quan), pow(M_E, float(x)));
+		printf("%.10f %.10f \n", sum(slag_exp, x, quan), pow(M_E,x));
+		printf("%.10f %.10f \n", r_sum(r_slag_exp, x, the_last_of_us(slag_exp, x, &quan), quan), pow(M_E, x));
 	}
 	}
 	return 0;
