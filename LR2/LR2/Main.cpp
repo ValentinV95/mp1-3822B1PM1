335c80a844e97f0ac7ff2e0ff70b618dd5966847
ARBUZNIJ <120045631+ARBUZNIJ@users.noreply.github.com>
2023-04-16 12:03:02 +0300

    Update Main.cpp
    precision == %.8f

--- LR2/LR2/Main.cpp
+++ LR2/LR2/Main.cpp
@@ -19,26 +19,26 @@ int main()
 	{
 	case 1:
 	{
-		printf("%.10f %.10f \n", sum(slag_sin, x, quan), sin(x));
-		printf("%.10f %.10f \n", r_sum(r_slag_sin, x, the_last_of_us(slag_sin, x, &quan), quan), sinf(x));
+		printf("%.8f %.8f \n", sum(slag_sin, x, quan), sin(x));
+		printf("%.8f %.8f \n", r_sum(r_slag_sin, x, the_last_of_us(slag_sin, x, &quan), quan), sinf(x));
 		break;
 	}
 	case 2:
 	{
-		printf("%.10f %.10f \n", sum(slag_cos, x, quan), cos(x));
-		printf("%.10f %.10f \n", r_sum(r_slag_cos, x, the_last_of_us(slag_cos, x, &quan), quan), cosf(x));
+		printf("%.8f %.8f \n", sum(slag_cos, x, quan), cos(x));
+		printf("%.8f %.8f \n", r_sum(r_slag_cos, x, the_last_of_us(slag_cos, x, &quan), quan), cosf(x));
 		break;
 	}
 	case 3:
 	{
-		printf("%.10f %.10f \n", sum(slag_ln, x, quan), log(x + 1.0f));
-		printf("%.10f %.10f \n", r_sum(r_slag_ln, x, the_last_of_us(slag_ln, x, &quan), quan), logf(x + 1.0f));
+		printf("%.8f %.8f \n", sum(slag_ln, x, quan), log(x + 1.0f));
+		printf("%.8f %.8f \n", r_sum(r_slag_ln, x, the_last_of_us(slag_ln, x, &quan), quan), logf(x + 1.0f));
 		break;
 	}
 	case 4:
 	{
-		printf("%.10f %.10f \n", sum(slag_exp, x, quan), pow(M_E,x));
-		printf("%.10f %.10f \n", r_sum(r_slag_exp, x, the_last_of_us(slag_exp, x, &quan), quan), powf(M_E, x));
+		printf("%.8f %.8f \n", sum(slag_exp, x, quan), pow(M_E,x));
+		printf("%.8f %.8f \n", r_sum(r_slag_exp, x, the_last_of_us(slag_exp, x, &quan), quan), powf(M_E, x));
 	}
 	}
 	return 0;
