4e97fa6adeef336910659d2b0e2cf81f188227cd
Andrey Kirillov <kirillov.andrey.123.15@gmail.com>
2023-05-26 00:40:04 +0300

    add lab Gaus

---
+++ lab Gaus/Main.cpp
@@ -0,0 +1,47 @@
+﻿#include "iostream"
+#include "Matrix.h"
+#include "Solver.h"
+#include "Vector.h"
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int choice, n;
+	std::cout << "Введите размер квадратной матрицы: ";
+	std::cin >> n;
+	std::cout << "Выберите тип данных: float-1, float-2: ";
+	std::cin >> choice;
+	try
+	{
+		switch (choice) {
+		case 1: {
+			Matrix<float> A(n,n);
+			Vector<float> b(n), x(n);
+			Solver<float> s;
+			std::cout << "Введите элементы матрицы A" << std::endl;
+			A.Scan();
+			std::cout << "Введите элементы матрицы b" << std::endl;
+			b.Scan();
+			s.Gaus(A, b, x);
+			std::cout << "Ответ" << std::endl;
+			x.Print();
+			break;
+		}
+		case 2: {
+			Matrix<double> A(n, n);
+			Vector<double> b(n), x(n);
+			Solver<double> s;
+			std::cout << "Введите элементы матрицы A" << std::endl;
+			A.Scan();
+			std::cout << "Введите элементы матрицы b" << std::endl;
+			b.Scan();
+			s.Gaus(A, b, x);
+			x.Print();
+			break;
+		}
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
---
+++ lab Gaus/Solver.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "Matrix.h"
+#include "Vector.h"
+
+template <class T>
+class Solver {
+public:
+	void Gaus(Matrix<T>& A, Vector<T>& b, Vector<T>& x) {
+		for (int i = 0; i < A.GetM(); i++) {
+			T largest_el = A.GetValue(i, i);
+			int m_largest = i;
+			for (int j = i; j < A.GetN(); j++) {
+				if (std::abs(A.GetValue(j, i)) > std::abs(largest_el)) {
+					largest_el = A.GetValue(j, i);
+					m_largest = j;
+				}
+			}
+
+			if (largest_el != 0) {
+				T divider = largest_el;
+				b.Swap(i, m_largest);
+				A.Swap(i, m_largest);
+
+				for (int j = 0; j < A.GetN(); j++) {
+					A.SetValue(i, j, A.GetValue(i, j) / divider);
+				}
+				b.SetValue(i, b.GetValue(i) / divider);
+			}
+			
+			for (int j = 0; j < A.GetM(); j++) {
+				T mult = A.GetValue(j, i);//mnozhitel
+				if (j == i) {
+					continue;
+				}
+				else {
+					for (int k = i; k < A.GetN(); k++) {
+						A.SetValue(j, k, A.GetValue(j, k) - A.GetValue(i, k) * mult);
+					}
+					b.SetValue(j, b.GetValue(j) - b.GetValue(i) * mult);
+				}
+			}
+		}
+		for (int i = 0; i < b.GetSize(); i++) {
+			x[i] = b[i];
+		}
+
+		for (int i = 0; i < A.GetM(); i++) {
+			if ((A.GetValue(i, i) == 0) && (x.GetValue(i)==0)) {
+				throw std::exception("СЛОУ имеет бусконечно много решений");
+			}
+		}
+		for (int i = 0; i < A.GetM(); i++) {
+			if (A.GetValue(i, i) == 0) {
+				throw std::exception("СЛОУ не имеет решений");
+			}
+		}
+	}
+};
