70427285b540eab22c838d54b2a573aacf34f09b
Petr Zaznobin <69428320+SmartyGeeky@users.noreply.github.com>
2023-05-25 21:02:53 +0300

    Update vector.h

--- Gauss/gauss/vector.h
+++ Gauss/gauss/vector.h
@@ -2,20 +2,41 @@
 #include <iostream>
 
 using namespace std;
-template <typename T>
+template<typename T>
 class vec {
 private:
     int n;
-    T* arr;
+    T *arr;
 public:
-    vec(int _n);
+    vec(int _n) {
+        n = _n;
+        arr = new T[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = -2;
+        }
+    }
 
-    T& operator [](int ind);
+    T &operator[](int ind) {
+        return arr[ind];
+    }
 
-    void set_vec(int _n);
+    void set_vec(int _n) {
+        n = _n;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+    }
 
-    void get_vec(int _n);
+    void get_vec(int _n) {
+        n = _n;
+        for (int i = 0; i < n; i++) {
+            cout << "( " << arr[i] << " )\n";
+        }
+    }
 
-    ~vec();
+    ~vec() {
+        delete[] arr;
+        n = 0;
+    }
 };
 
