b1a0dbcc828f7f1c7c809d365b1f37b0d7c9fc39
Petr Zaznobin <69428320+SmartyGeeky@users.noreply.github.com>
2023-05-26 09:34:54 +0300

    Add files via upload

---
+++ gauss/gauss/gauss.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "funcs.h"
+#include "matrix.h"
+#include "vec.h"
+
+using namespace std;
+
+int main() {
+    int choice;
+    int n;
+    cout << "Enter typename:\n 1. double;\n 2. float;\n 3. int\n";
+    cin >> choice;
+    cout << "Enter size: ";
+    cin >> n;
+    switch (choice) {
+    case 1:
+        solver<double>(n);
+        break;
+    case 2:
+        solver<float>(n);
+        break;
+    default:
+        return 0;
+    }
+
+}
---
+++ gauss/gauss/matrix.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <iostream>
+using namespace std;
+template<typename T>
+class matrix {
+private:
+    T** arr;
+    int n; //размерность
+public:
+    matrix(int _n) {
+        n = _n;
+        arr = new T * [n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new T[n];
+        }
+    }
+
+    T& operator()(int ind1, int ind2) {
+        return arr[ind1][ind2];
+    }
+
+    matrix(const matrix<T>& A) {
+        n = A.n;
+        arr = new T * [n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new T[n];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = A.arr[i][j];
+            }
+        }
+    }
+
+
+    void set_matrix(int _n) {
+        n = _n;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> arr[i][j];
+            }
+        }
+    };
+
+    void get_matrix(int _n) {
+        n = _n;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << arr[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+
+    ~matrix() {
+        for (int i = 0; i < n; i++) {
+            delete[] arr[i];
+        }
+        delete[] arr;
+        n = 0;
+    }
+};
+
+
+
+
