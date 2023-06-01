14f6a511c86db747397b0f3b8a6bf2c9a25fa0c7
Andrey Kirillov <kirillov.andrey.123.15@gmail.com>
2023-06-01 22:27:20 +0300

    update main and solver

--- lab Gaus/Main.cpp
+++ lab Gaus/Main.cpp
@@ -3,13 +3,18 @@
 #include "Solver.h"
 #include "Vector.h"
 
+template <typename T>
+void rand_slou(Matrix<T>& A, Vector<T>& b);
+
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int choice, n;
+	int choice, n, choice_rand;
 	std::cout << "Введите размер квадратной матрицы: ";
 	std::cin >> n;
-	std::cout << "Выберите тип данных: float-1, float-2: ";
+	std::cout << "Выберите тип данных: float-1, double-2: ";
 	std::cin >> choice;
+	std::cout << "Выберите тип заполнения: рандом-1, самостоятельно-2: ";
+	std::cin >> choice_rand;
 	try
 	{
 		switch (choice) {
@@ -17,10 +22,17 @@ int main() {
 			Matrix<float> A(n,n);
 			Vector<float> b(n), x(n);
 			Solver<float> s;
-			std::cout << "Введите элементы матрицы A" << std::endl;
-			A.Scan();
-			std::cout << "Введите элементы матрицы b" << std::endl;
-			b.Scan();
+			if (choice_rand == 1) {
+				rand_slou(A, b);
+				A.Print();
+				b.Print();
+			}
+			else {
+				std::cout << "Введите элементы матрицы A" << std::endl;
+				A.Scan();
+				std::cout << "Введите элементы матрицы b" << std::endl;
+				b.Scan();
+			}
 			s.Gaus(A, b, x);
 			std::cout << "Ответ" << std::endl;
 			x.Print();
@@ -30,10 +42,17 @@ int main() {
 			Matrix<double> A(n, n);
 			Vector<double> b(n), x(n);
 			Solver<double> s;
-			std::cout << "Введите элементы матрицы A" << std::endl;
-			A.Scan();
-			std::cout << "Введите элементы матрицы b" << std::endl;
-			b.Scan();
+			if (choice_rand == 1) {
+				rand_slou(A, b);
+				A.Print();
+				b.Print();
+			}
+			else {
+				std::cout << "Введите элементы матрицы A" << std::endl;
+				A.Scan();
+				std::cout << "Введите элементы матрицы b" << std::endl;
+				b.Scan();
+			}
 			s.Gaus(A, b, x);
 			x.Print();
 			break;
@@ -43,5 +62,15 @@ int main() {
 	catch (const std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
+	}
+}
+
+template <typename T>
+void rand_slou(Matrix<T>& A, Vector<T>& b) {
+	for (int i = 0; i < A.GetM(); i++) {
+		for (int j = 0; j < A.GetN(); j++) {
+			A.SetValue(i, j, ((T)rand()) / 100);
+		}
+		b.SetValue(i, ((T)rand()) / 100);
 	}
 }
--- lab Gaus/Solver.h
+++ lab Gaus/Solver.h
@@ -6,7 +6,10 @@ template <class T>
 class Solver {
 public:
 	void Gaus(Matrix<T>& A, Vector<T>& b, Vector<T>& x) {
-		double eps =std::pow(10, -6);
+		double eps = 10e-7;
+		if (sizeof(T) == 8) {
+			eps = 10e-16;
+		}
 		for (int i = 0; i < A.GetM(); i++) {
 			T largest_el = A.GetValue(i, i);
 			int m_largest = i;
@@ -16,11 +19,13 @@ public:
 					m_largest = j;
 				}
 			}
-
 			if (std::abs(largest_el) < eps) {
-				throw std::exception("СЛОУ имеет бусконечно много решений");
+				if (b.GetValue(m_largest) < eps) {
+					b.SetValue(m_largest, 0);
+				}
+				A.SetValue(m_largest, i, 0);
+				continue;
 			}
-
 			if (largest_el != 0) {
 				T divider = largest_el;
 				b.Swap(i, m_largest);
@@ -31,7 +36,6 @@ public:
 				}
 				b.SetValue(i, b.GetValue(i) / divider);
 			}
-			
 			
 			for (int j = 0; j < A.GetM(); j++) {
 				T mult = A.GetValue(j, i);//mnozhitel
@@ -46,13 +50,14 @@ public:
 				}
 			}
 		}
+
 		for (int i = 0; i < b.GetSize(); i++) {
 			x[i] = b[i];
 		}
 
 		for (int i = 0; i < A.GetM(); i++) {
 			if ((A.GetValue(i, i) == 0) && (x.GetValue(i)==0)) {
-				throw std::exception("СЛОУ имеет бусконечно много решений");
+				throw std::exception("СЛОУ имеет бесконечно много решений");
 			}
 		}
 		for (int i = 0; i < A.GetM(); i++) {
